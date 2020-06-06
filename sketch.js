da1f0caee616152f1e8e674b8ae695835cf47f72
Altaf Sarker <ahsarker@AHSarker-M07.local>
2020-06-05 19:51:00 -0700

    sketch

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
 
-
+var gameState = "onSling";
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -69,16 +69,19 @@ function draw(){
 }
 
 function mouseDragged(){
+    if(gameState!=="launched"){
     Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    }
 }
 
 
 function mouseReleased(){
+    gameState = "launched";
     slingshot.fly();
 }
 
 function keyPressed(){
     if(keyCode === 32){
-        slingshot.attach(bird.body);
+        //slingshot.attach(bird.body);
     }
 }
